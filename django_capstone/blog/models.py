579ad37d61464ca5899cd3a4e0ab2a762e801ea9
lukho <plamlukho1@gmail.com>
2024-01-23 11:14:56 +0200

    added docstring to models.py

--- django_capstone/blog/models.py
+++ django_capstone/blog/models.py
@@ -3,6 +3,17 @@ from django.db import models
 
 # Create your models here.
 class Post(models.Model):
+    """
+    A class to represent a post
+    ...
+    Atrributes
+    ----------
+    id : int
+    title : str
+    body :
+    date :
+    signature : 
+    """
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
     body = models.TextField()
