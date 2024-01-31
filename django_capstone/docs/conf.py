c66b382ef122da7b55699e20561781cb7baf156d
lukho <plamlukho1@gmail.com>
2024-01-31 16:08:56 +0200

    added autodoc in cof.py

--- django_capstone/docs/conf.py
+++ django_capstone/docs/conf.py
@@ -20,7 +20,9 @@ author = 'lukho'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
