e7f848f92d39b82c55f37e140b7487b437e8c54e
lukho <plamlukho1@gmail.com>
2024-01-31 15:48:49 +0200

    sphinx docs

---
+++ django_capstone/docs/_build/html/_sources/installation.rst.txt
@@ -0,0 +1,11 @@
+Installation
+============
+
+To install My Project, you can use `pip`. Open a terminal and run:
+
+.. code-block:: bash
+
+    pip install django_project
+
+This will install the latest version of My Project and its dependencies.
+
--- django_capstone/docs/conf.py
+++ django_capstone/docs/conf.py
@@ -2,7 +2,7 @@ import os
 import sys
 import django
 sys.path.insert(0, os.path.abspath('..'))
-os.environ['DJANGO_SETTINGS_MODULE'] = 'malema.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'django_capstone.settings'
 django.setup()
 
 # Configuration file for the Sphinx documentation builder.
